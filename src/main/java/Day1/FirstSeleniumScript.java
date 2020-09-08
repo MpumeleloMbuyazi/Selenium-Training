5c1b601750c896691390a35e489a877079a298e3
Mpumelelo Dube <mdube@entersekt.com>
2020-09-08 15:58:38 +0200

    Add new selenium repo

---
+++ src/main/java/Day1/FirstSeleniumScript.java
@@ -0,0 +1,49 @@
+package Day1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FirstSeleniumScript {
+
+    public static void main(String[] args) {
+
+        WebDriver driver;
+
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\mdube\\Documents\\Selenium-Training\\Resources\\chromedriver.exe");
+
+        //Create a new instance of the Chrome driver
+        driver = new ChromeDriver();
+
+        String URL = "http://automationpractice.com ";
+        driver.get(URL);
+
+        //Maximize chrome browser
+        driver.manage().window().maximize();
+
+        //Storing title name in the String variable
+        String title = driver.getTitle();
+
+        //Printing Title in console
+        System.out.println("Title of the page is: " + title);
+
+        //Storing URL in String variable
+        String actualURL = driver.getCurrentUrl();
+
+        //Verify that the urls match by printing a message
+        if(actualURL.contains(URL)){
+
+            System.out.println("Pass");
+            
+        }else {
+
+            System.out.println("Fail");
+
+            //Print the actual and expected URL for record purposes
+            System.out.println("Actual URL is: " + actualURL);
+            System.out.println("Expected URL is: " + URL);
+        }
+
+        driver.close();
+
+    }
+}
